7f8700de701fc7dede3226fa48fbb0f76bc389d6
fredmloth <frederiekeloth@gmail.com>
2025-03-03 12:25:58 +0100

    overall structure before implementing pseudocode

---
+++ 2c.py
@@ -0,0 +1,31 @@
+"""This module answers assignment 2.3 and will later likely be divided 
+into seperate modules"""
+
+import numpy as np
+
+def laplacian_roll():
+    """Computes the laplacian by implementing np.roll"""
+    return
+
+def boundary_neumann():
+    """Implements the Neumann Boundary Conditions"""
+    return
+
+def gray_scott():
+    """Calculates The gray scott reaction-diffusion equations."""
+    return
+
+def update():
+    """Updates the grid for U and V (seperate grids)"""
+
+def plot():
+    """plots concentrations U and V
+    - For several parameter choices
+    - Multiple graphs below for different parameter changes?
+    - Can find sets for specific parameter settings online
+        - Change those and note trends/changes in structure/ effects"""
+
+# Optional
+def implement_mask():
+    """Implement a mask
+    - For which parameters?"""
