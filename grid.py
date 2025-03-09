4a758f66fcf159d0ede764252c3d9f7eac9037ff
liesbeto <lies.ooghe@gmail.com>
2025-03-09 17:09:31 +0100

    docstrings in classes

--- grid.py
+++ grid.py
@@ -4,28 +4,54 @@ import matplotlib.pyplot as plt
 from random_walker import Walker
 
 class Grid():
-    def __init__(self, grid_size, sticking_probability, size_cluster):
-        self.grid_size = grid_size
-        self.stick_prob = sticking_probability
-        self.size_cluster = size_cluster
+    """
+    This class-object creates a domain-grid and cluster to investigate the use
+    of the Monte Carlo method for DLA.
+    """
 
-        # i assume the initial cluster is a single candidate ~ in the middle at the bottom?
-        self.cluster = [[int(grid_size/2), 1]]
+    def __init__(
+            self,
+            grid_size: int,
+            sticking_probability: float,
+            size_cluster: int):
+        """
+        Initialises the Grid class-object and creates initial cluster
+        containing a single particle in the middle at the bottom of the domain.
+
+        Parameters:
+        - grid_size (int): Size of the grid (grid_size x grid_size).
+        - stick_prob (float): The sticking probability of each particle to the
+          cluster (0 < p <= 1).
+        - size_cluster (int): The desired size of the cluster.
+        """
+        self.grid_size: int = grid_size
+        self.stick_prob: float = sticking_probability
+        self.size_cluster: int = size_cluster
+        self.cluster: list[list[int]] = [[int(grid_size/2), 1]]
 
     def create_cluster(self):
+        """
+        Creates Walker class-objects that act as particles behaving with
+        Brownian Motion until the desired size of the cluster is reached.
+        """
         while self.check_size() < self.size_cluster:
             random_walker = Walker(self)
             while random_walker.active:
                 random_walker.walk(self)
                 random_walker.check_cluster(self)
-        return self
     
     def check_size(self):
+        """
+        Returns the number of aggregated particles of the cluster.
+        """
         return len(self.cluster)
 
     def visualise(self):
-        for growth_candidate in self.cluster:
-            plt.plot(growth_candidate[0], growth_candidate[1], "ro", markersize = 2)
+        """
+        Plots a visual representation of the cluster on the domain.
+        """
+        for aggre_part in self.cluster:
+            plt.plot(aggre_part[0], aggre_part[1], "ro", markersize = 1)
         plt.axis("square")
         plt.xlim(0,101)
         plt.ylim(0,101)
@@ -33,12 +59,26 @@ class Grid():
         plt.show()
 
     def print_stats(self):
+        """
+        Prints some information about the cluster.
+        """
         print(f"size: {len(self.cluster)}")
         max_y = 0
         min_x, max_x = int(self.grid_size/2), int(self.grid_size/2)
-        for growth_candidate in self.cluster:
-            if growth_candidate[0] < min_x: min_x = growth_candidate[0]
-            if growth_candidate[0] > max_x: max_x = growth_candidate[0]
-            if growth_candidate[1] > max_y: max_y = growth_candidate[1]
+        for aggre_part in self.cluster:
+            if aggre_part[0] < min_x: min_x = aggre_part[0]
+            if aggre_part[0] > max_x: max_x = aggre_part[0]
+            if aggre_part[1] > max_y: max_y = aggre_part[1]
         print(f"width: {max_x - min_x}")
         print(f"height: {max_y - 1}")
+
+    def check_spread(self):
+        """
+        Returns the height and width of the cluster.
+        """
+        max_y, min_x, max_x = 1, int(self.grid_size/2), int(self.grid_size/2)
+        for aggre_part in self.cluster:
+            if aggre_part[0] < min_x: min_x = aggre_part[0]
+            if aggre_part[0] > max_x: max_x = aggre_part[0]
+            if aggre_part[1] > max_y: max_y = aggre_part[1]
+        return max_y - 1, max_x - min_x
--- random_walker.py
+++ random_walker.py
@@ -3,15 +3,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Walker():
+    """
+    This class-object creates a particle that behaves as a random walker with
+    periodic boundary conditions on the left and right border and regeneration
+    on the top and bottom borders.
+    """
+
     def __init__(self, grid):
-        self.active = True
-        self.growth_candidate = False
-        self.location = [random.randint(1, grid.grid_size), grid.grid_size]
-        self.movements = []
-        self.movements.append(self.location[:])
+        """
+        Initialises the Walker class-object by giving it an initial random
+        location on the top border.
+
+        Parameters:
+        - grid (Grid): Uses grid.grid_size to put the initial location within
+          the boundaries of the domain.
+        """
+        self.active: bool = True
+        self.clustered: bool = False
+        self.location: list[int] = [random.randint(1, grid.grid_size), grid.grid_size]
+        self.movements: list[list[int]] = [self.location[:]]
 
     def walk(self, grid):
-        
+        """
+        The walker object takes a step in any direction.
+
+        Parameters:
+        - grid (Grid): Uses grid.grid_size to check whether walker moves into a
+          border and uses grid.cluster to check whether walker wishes to move
+          into the cluster (possible when stick_prob < 1).
+        """
+
         # if direction is 0, walker moves in x-direction.
         # if direction is 1, walker moves in y-direction.
         direction = random.choice([0, 1])
@@ -37,15 +58,31 @@ class Walker():
         return self.location
 
     def check_cluster(self, grid):
-        for growth_candidate in grid.cluster:
-            if (abs(self.location[0] - growth_candidate[0]) + abs(self.location[1] - growth_candidate[1])) == 1 and self.active == True and random.random() <= grid.stick_prob:
-                self.growth_candidate = True
+        """
+        If Walker object is adjacent to cluster, becomes part of cluster with
+        likelihood of sticking probability.
+
+        Parameters:
+        - grid (Grid): Uses grid.stick_prob and grid.cluster to add particle to
+          list of aggregated particles.
+
+        Returns True if Walker becomes part of cluster and returns False
+        otherwise.
+        """
+        for aggre_part in grid.cluster:
+            if (abs(self.location[0] - aggre_part[0]) + abs(self.location[1] - aggre_part[1])) == 1 and self.active == True and random.random() <= grid.stick_prob:
+                self.clustered = True
                 self.active = False
                 grid.cluster.append(self.location)
                 return True
         return False
     
     def visualise_walk(self, grid):
+        """
+        Plots path of particle until it becomes inactive (either due to moving
+        into top or bottom boundary or due to becoming part of cluster) as a
+        check of the path.
+        """
         print(self.movements)
         x, y = [], []
 
